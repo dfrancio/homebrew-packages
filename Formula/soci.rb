6be00c05ad120fb9b5eefea0b97cabd48a157994
Diego Sogari <diego.sogari@gmail.com>
2017-03-14 01:23:33 -0300

    Update options for package soci

--- Formula/soci.rb
+++ Formula/soci.rb
@@ -4,34 +4,21 @@ class Soci < Formula
   head "https://github.com/SOCI/soci.git",
     :revision => "e719c44bb89949d3c179a6390de57ccd9046878b"
 
+  option "with-odbc", "Builds with ODBC support"
+  option "with-mysql", "Builds with MySQL support"
+
   depends_on "cmake" => :build
   depends_on "boost" => :optional
   depends_on "sqlite" => :optional
   depends_on "postgresql" => :optional
-  depends_on "unixodbc" => :optional
-  depends_on "libiodbc" => :optional
-  depends_on "mariadb-connector-c" => :optional
-  depends_on "mysql-connector-c" => :optional
+  depends_on "unixodbc" if build.with? "odbc"
+  depends_on "mariadb-connector-c" if build.with? "mysql"
 
   def translate(a)
-    case a
-    when "sqlite" then "sqlite3"
-    when "unixodbc" then "odbc"
-    when "libiodbc" then "odbc"
-    when "mariadb-connector-c" then "mysql"
-    when "mysql-connector-c" then "mysql"
-    else a
-    end
+    a == "sqlite" ? "sqlite3" : a
   end
 
   def install
-    if build.with?("unixodbc") && build.with("libiodbc")
-      odie "Options --with-unixodbc and --with-libiodbc are mutually exclusive."
-    end
-    if build.with?("mariadb-connector-c") && build.with("mysql-connector-c")
-      odie "Options --with-mariadb-connector-c and --with-mysql-connector-c are mutually exclusive."
-    end
-
     inreplace "./cmake/SociConfig.cmake" do |s|
       s.gsub! /-pedantic -Werror/, ""
       s.gsub! /-Wredundant-decls/, ""
@@ -41,7 +28,7 @@ class Soci < Formula
 
     args = %w[-DSOCI_TESTS=OFF]
 
-    %w[boost sqlite postgresql unixodbc libiodbc mariadb-connector-c mysql-connector-c].each do |a|
+    %w[boost sqlite postgresql odbc mysql].each do |a|
       bool = build.with?(a) ? "ON" : "OFF"
       args << "-DWITH_#{translate(a).upcase}:BOOL=#{bool}"
     end
@@ -50,8 +37,8 @@ class Soci < Formula
       args << "-DSQLITE3_INCLUDE_DIR=/usr/local/opt/sqlite3/include"
       args << "-DSQLITE3_LIBRARIES=/usr/local/opt/sqlite3/lib/libsqlite3.dylib"
     end
-  
-    if build.with? "unixodbc"
+
+    if build.with? "odbc"
       args << "-DODBC_INCLUDE_DIR=/usr/local/include"
       args << "-DODBC_LIBRARIES=/usr/local/lib/libodbc.dylib"
     end
@@ -60,8 +47,8 @@ class Soci < Formula
       args << "-DPostgreSQL_INCLUDE_DIR=/usr/local/include"
       args << "-DPostgreSQL_LIBRARIES=/usr/local/lib/libpq.dylib"
     end
-  
-    if build.with? "mariadb-connector-c"
+
+    if build.with? "mysql"
       args << "-DMYSQL_INCLUDE_DIR=/usr/local/include/mariadb"
       args << "-DMYSQL_LIBRARIES=/usr/local/lib/mariadb/libmysqlclient.dylib"
     end
