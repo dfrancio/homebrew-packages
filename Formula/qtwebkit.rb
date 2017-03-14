922cb24f540701b472bf92bb2daf08450640601f
Diego Sogari <diego.sogari@gmail.com>
2017-03-14 16:39:53 -0300

    Fix formula for package qtwebkit

--- Formula/qtwebkit.rb
+++ Formula/qtwebkit.rb
@@ -1,21 +1,57 @@
 class Qtwebkit < Formula
-  desc "Client and server components which implement a crash-reporting system"
+  desc "Open Source Web Browser Engine - Qt"
   homepage "https://webkit.org"
-  url "https://github.com/annulen/webkit/releases/download/qtwebkit-tp5/qtwebkit-tp5.tar.xz"
-  sha256 "553dfb47a10d256314b74105d0eafadab0efc0ebd32d77a09cffbe2fed09def8"
   head "https://github.com/annulen/webkit.git"
 
+  devel do
+    url "https://github.com/annulen/webkit/releases/download/qtwebkit-tp5/qtwebkit-tp5.tar.xz"
+    version "5.602.3"
+    sha256 "553dfb47a10d256314b74105d0eafadab0efc0ebd32d77a09cffbe2fed09def8"
+  end
+
+  keg_only "Same as Qt 5"
+
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
   depends_on "qt5" => :run
-  depends_on "libjpeg" => :run
+  depends_on "jpeg" => :run
   depends_on "libpng" => :run
-  depends_on "webp" => :optional
-  depends_on "fontconfig" => :optional
-  depends_on "pkg-config" => :optional
+  depends_on "webp" => :recommended
+  depends_on "fontconfig" => :recommended
 
   def install
     system "cmake", ".", *std_cmake_args, "-DPORT=Qt"
     system "make"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"hello.pro").write <<-EOS.undent
+      QT       += core qtwebkit qtwebkitwidgets
+      QT       -= gui
+      TARGET    = hello
+      CONFIG   += console
+      CONFIG   -= app_bundle
+      TEMPLATE  = app
+      SOURCES  += main.cpp
+    EOS
+
+    (testpath/"main.cpp").write <<-EOS.undent
+      #include <QCoreApplication>
+      #include <QDebug>
+      #include <QWebKit>
+      int main(int argc, char *argv[])
+      {
+        QCoreApplication a(argc, argv);
+        qDebug() << "Hello World!";
+        return 0;
+      }
+    EOS
+
+    system bin/"qmake", testpath/"hello.pro"
+    system "make"
+    assert File.exist?("hello")
+    assert File.exist?("main.o")
+    system "./hello"
   end
 end
