8207eaed3be35ad7bea665713dd702e3fd8002df
Diego Sogari <diego.sogari@gmail.com>
2017-03-21 00:59:34 -0300

    Bottle package CEF

--- Formula/cef.rb
+++ Formula/cef.rb
@@ -6,6 +6,11 @@ class Cef < Formula
   sha256 "0aadaa47c10119195e782cf138de4c3e05a717c5f3e32e78004020e7b428c062"
   head "https://bitbucket.org/chromiumembedded/cef.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "30424a98ef8c85f7a9e36e639921b74f2cbb0b29a031ff261f13d94a5ebde41c" => :sierra
+  end
+
   depends_on "cmake" => :build
 
   keg_only "Intended for development of CEF-based applications. Do not 'brew link' this software!"
