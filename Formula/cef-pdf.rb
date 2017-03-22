273391d08690fbece0a50499e9b0d9ca82ff9bf5
Diego Sogari <diego.sogari@gmail.com>
2017-03-22 05:07:04 -0300

    Update cef-pdf build

--- Formula/cef-pdf.rb
+++ Formula/cef-pdf.rb
@@ -1,16 +1,20 @@
 class CefPdf < Formula
   desc "HTML to PDF utility based on CEF"
   homepage "https://github.com/spajak/cef-pdf"
-  url "https://github.com/spajak/cef-pdf/archive/v0.2.1.tar.gz"
-  sha256 "2663e45c67dd81f1f9243a12489b177a5cee8fa6f1dd33345088173a5d02f2fe"
-  head "https://github.com/spajak/cef-pdf.git"
-
-  keg_only "Intended for development of CEF-based applications. Do not 'brew link' this software!"
+  url "https://github.com/dsogari/cef-pdf/archive/v0.3.0.tar.gz"
+  sha256 "79279128bbf21a1cb6b136fde5de8545c05f4550acc10fd568c1654f26ceba4e"
+  head "https://github.com/dsogari/cef-pdf.git"
 
   depends_on "cmake" => :build
-  depends_on "cef" => :build
+  depends_on "dsogari/packages/cef" => :build
 
   def install
-    
+    cef_root = Formula["dsogari/packages/cef"].opt_prefix
+    system "cmake", ".", *std_cmake_args,
+      "-DBUILD_SHARED_LIBS=ON",
+      "-DCEF_ROOT=#{cef_root}"
+    system "make"
+
+    prefix.install "src/Release/cef-pdf.app"
   end
 end
