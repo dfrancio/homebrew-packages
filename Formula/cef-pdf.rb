4c403a369442e67f708693daf46c09a7c9a90588
Diego Sogari <diego.sogari@gmail.com>
2017-03-24 03:50:15 -0300

    Fix build of cef-pdf

--- Formula/cef-pdf.rb
+++ Formula/cef-pdf.rb
@@ -2,12 +2,8 @@ class CefPdf < Formula
   desc "HTML to PDF utility based on CEF"
   homepage "https://github.com/spajak/cef-pdf"
   url "https://github.com/dsogari/cef-pdf/archive/v0.3.0.tar.gz"
-  sha256 "c56c6fab59586837cab39e72731701b6a1b7256925eca52b77071c3c802597a0"
+  sha256 "ff1b8ee61d574c09ce3774894e92fd5eadf33d87d3c2225ebdeb32cacebc756c"
   head "https://github.com/dsogari/cef-pdf.git"
-
-  bottle do
-    sha256 "b7fe1e76daa0c0c0bb53b3d444a93e6cd36d511e77b4457355dcf8e0368e6969" => :sierra
-  end
 
   depends_on "cmake" => :build
   depends_on "dsogari/packages/cef" => :build
