7b3385764af22cd2eecd68837a33b0537f5d1506
Diego Sogari <diego.sogari@gmail.com>
2017-03-24 03:55:48 -0300

    Bottle rebuild of cef-pdf

--- Formula/cef-pdf.rb
+++ Formula/cef-pdf.rb
@@ -5,6 +5,11 @@ class CefPdf < Formula
   sha256 "ff1b8ee61d574c09ce3774894e92fd5eadf33d87d3c2225ebdeb32cacebc756c"
   head "https://github.com/dsogari/cef-pdf.git"
 
+  bottle do
+    rebuild 1
+    sha256 "1ee3d4f932822a8a5a837c47fb65d97f0e58fc99ab324d741c2988fe46ee42ba" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "dsogari/packages/cef" => :build
   depends_on "boost" => :run
