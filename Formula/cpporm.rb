1a1556c7cc2e0b20ab1e057f9380fca74193f388
Diego Sogari <diego.sogari@gmail.com>
2017-03-25 21:30:44 -0300

    Update cporm package

--- Formula/cpporm.rb
+++ Formula/cpporm.rb
@@ -1,21 +1,15 @@
 class Cpporm < Formula
   desc "Simple ORM library with SQL parser"
   homepage "https://github.com/dsogari/cpporm"
-  url "https://github.com/dsogari/cpporm/archive/v0.2.16.tar.gz"
-  sha256 "81fdcd06d2305879c080e44dcaf681bbdebcf31e0facb924a42c0f12c948eedd"
+  url "https://github.com/dsogari/cpporm/archive/v0.2.17.tar.gz"
+  sha256 "e9a75e4f943d56fd23a7fd089cb1606ca6a2ec6f111a5a5209bfcfaa29a1d01f"
   head "https://github.com/dsogari/cpporm.git"
-
-  bottle do
-    cellar :any
-    root_url "https://github.com/dsogari/homebrew-packages/raw/master/Bottle"
-    sha256 "ac22f70a2af52a751b1645fe1d0a1a84d71518f3b17704d1e618a3e6cf4e8e74" => :sierra
-  end
 
   depends_on "cmake" => :build
   depends_on "boost" => :run
   depends_on "gflags" => :run
-  depends_on "soci" => :run
   depends_on "antlr4-runtime-cpp" => :run
+  depends_on "dsogari/packages/soci" => :run
 
   def install
     args = %w[
