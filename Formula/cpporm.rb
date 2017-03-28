65c9574672850b5c396933ffeb4843974ab98937
Diego Sogari <diego.sogari@gmail.com>
2017-03-28 02:47:10 -0300

    Bottle cpporm

--- Formula/cpporm.rb
+++ Formula/cpporm.rb
@@ -5,6 +5,12 @@ class Cpporm < Formula
   sha256 "7d019b77d4155c8ba7149009a93549b9c8b2245a168a2272988c57c3ff850a5c"
   head "https://github.com/dsogari/cpporm.git"
 
+  bottle do
+    cellar :any
+    root_url "https://github.com/dsogari/homebrew-packages/raw/master/Bottle"
+    sha256 "aa6398225e4e34e16edd96df2250c001f45af7ef0b14eb5f4fa4b717ff26ab6a" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "boost" => :run
   depends_on "gflags" => :run
